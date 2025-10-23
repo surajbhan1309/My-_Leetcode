5e01b23b1eec311d332ae87d70c5e2a689d098e5
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 13:34:46 +0530

    [141] [Time Beats: 79.68%] [Memory Beats: 99.98%] - LeetPush

---
+++ Linked-List-Cycle.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* hasCycle(ListNode* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (slow != NULL && fast != NULL) {
+        fast = fast->next;
+
+        if (fast != NULL) {
+            fast = fast->next;
+        }
+
+        slow = slow->next;
+
+        if (slow == fast) {
+        return slow;
+        }
+    }
+    return NULL; 
+}
+};
