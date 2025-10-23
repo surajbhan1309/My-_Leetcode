595b6aad34aa213be24988b6f8eaa4d23d6a61b3
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 13:51:21 +0530

    [876] [Time Beats: 0.02%] [Memory Beats: 100.00%] - LeetPush

---
+++ Middle-of-the-Linked-List.cpp
@@ -0,0 +1,40 @@
+
+// class Solution {
+// public:
+//     int getLength(ListNode* head){
+//         int len=0;
+//         while(head!=NULL){
+//             len++;
+//             head=head->next;
+//         }
+//         return len;
+//     }
+    
+
+//     ListNode* middleNode(ListNode* head) {
+//         int len=getLength(head);
+//         int ans=(len/2);
+//         ListNode* temp=head;
+//         int cnt=0;
+//         while(cnt<ans){
+//             temp=temp->next;
+//             cnt++;
+//         }
+//         return temp;
+        
+//     }
+// };
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+};
