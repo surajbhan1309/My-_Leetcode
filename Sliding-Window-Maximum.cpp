7d696c9b989463fdb5d62dc6467380fb8f1c39ce
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 13:40:27 +0530

    [239] [Time Beats: 30.46%] [Memory Beats: 92.96%] - LeetPush

---
+++ Sliding-Window-Maximum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> maxi(k);
+        int n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i < k; i++) {
+            while (!maxi.empty() && nums[maxi.back()] <= nums[i]) {
+                maxi.pop_back();
+            }
+            maxi.push_back(i);
+        }
+        ans.push_back(nums[maxi.front()]);
+        for (int i = k; i < n; i++) {
+            while (!maxi.empty() && i - maxi.front() >= k) {
+                maxi.pop_front();
+            }
+            while (!maxi.empty() && nums[maxi.back()] <= nums[i]) {
+                maxi.pop_back();
+            }
+            maxi.push_back(i);
+            ans.push_back(nums[maxi.front()]);
+        }
+        return ans;
+    }
+};
