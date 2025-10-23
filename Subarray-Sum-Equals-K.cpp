1d305169733310308ed5921c7a7baa17f1135f84
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 13:46:14 +0530

    [560] [Time Beats: 41.25%] [Memory Beats: 10.68%] - LeetPush

---
+++ Subarray-Sum-Equals-K.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>prefix;
+        int sum=0,count=0;
+        prefix[0]=1;
+
+        for(int &num:nums){
+            sum+=num;
+            count+=prefix[sum-k];
+            prefix[sum]++;
+        }
+        return count;
+    }
+};
