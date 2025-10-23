dcdaa47e3523c07d9a6d20c37870ed61c98f8f80
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 14:00:08 +0530

    [2742] [Time Beats: 70.58%] [Memory Beats: 36.28%] - LeetPush

---
+++ Painting-the-Walls.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solvetab(vector<int>&cost,vector<int>&time,int index,int walls,vector<vector<int>>&dp){
+        if(walls<=0){
+            return 0;
+        }
+        if(index<0){
+            return 1e9;
+        }
+        if(dp[index][walls]!=-1){
+            return dp[index][walls];
+        }
+        int notTake=0+solvetab(cost,time,index-1,walls,dp);
+        int take=cost[index]+solvetab(cost,time,index-1,walls-time[index]-1,dp);
+        return dp[index][walls]=min(notTake,take);
+    }
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        int n=cost.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solvetab(cost,time,n-1,n,dp);
+    }
+};
