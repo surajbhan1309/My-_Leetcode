3d60c109ab74f44504bc014caa6ef5cff1ead670
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 13:43:25 +0530

    [416] [Time Beats: 11.02%] [Memory Beats: 50.70%] - LeetPush

---
+++ Partition-Equal-Subset-Sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[201][20001];
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum=accumulate(begin(nums),end(nums),0);
+        if(sum%2!=0){
+            return false;
+        }
+        memset(dp,-1,sizeof(dp));
+        int x=sum/2;
+        return solve(0,x,nums);
+    }
+    bool solve(int i,int target,vector<int>&nums){
+        if(target==0){
+            return true;
+        }
+        if(i>=nums.size()){
+            return false;
+        }
+        if(dp[i][target]!=-1){
+            return dp[i][target];
+        }
+        bool take=false;
+        if(nums[i]<=target){
+            take=solve(i+1,target-nums[i],nums);
+        }
+        bool not_take=solve(i+1,target,nums);
+
+        return dp[i][target]=take||not_take;
+
+    }
+};
