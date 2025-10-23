b398339d97ce9b0c5e6dcf58fc932fc993ed1667
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 13:41:15 +0530

    [268] [Time Beats: 6.65%] [Memory Beats: 99.94%] - LeetPush

---
+++ Missing-Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        for(i=0;i<nums.size();i++){
+            if(i!=nums[i]){
+                break;
+            }
+        }
+        return i;     
+    }
+};
