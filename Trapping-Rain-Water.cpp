1f8fc603859841a49a5634049a2c87f7dfe62c23
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-23 13:01:49 +0530

    [42] [Time Beats: 100.00%] [Memory Beats: 60.94%] - LeetPush

---
+++ Trapping-Rain-Water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int left=0,right=n-1;
+        int lmax=0,rmax=0;
+        int ans=0;
+        while(left<right){
+            lmax=max(lmax,height[left]);
+            rmax=max(rmax,height[right]);
+            if(lmax<rmax){
+                ans+=(lmax-height[left]);
+                left++;
+            }
+            else{
+                ans+=(rmax-height[right]);
+                right--;
+            }
+        }
+        return ans;
+        
+    }
+};
