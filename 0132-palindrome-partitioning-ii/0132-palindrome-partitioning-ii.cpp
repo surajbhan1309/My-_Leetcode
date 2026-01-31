c8c5b6fa1d52cf3c62870cd284c2f5ec76f35f16
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-01-31 17:38:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,37 +1,67 @@
 class Solution {
 public:
-    int minCut(string s) {
+    bool isPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
+    }
+
+    int solve(int i, string &s) {
         int n = s.size();
-        
-        // isPal[i][j] = true if s[i..j] is palindrome
-        vector<vector<bool>> isPal(n, vector<bool>(n, false));
-        
-        // Precompute palindromes
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-                if (s[i] == s[j]) {
-                    if (len <= 2 || isPal[i + 1][j - 1])
-                        isPal[i][j] = true;
-                }
+        if (i == n) return -1;   // no cut needed after last part
+
+        int ans = INT_MAX;
+
+        for (int j = i; j < n; j++) {
+            if (isPalindrome(s, i, j)) {
+                ans = min(ans, 1 + solve(j + 1, s));
             }
         }
-        
-        // dp[i] = min cuts for s[0..i]
-        vector<int> dp(n, INT_MAX);
-        
-        for (int i = 0; i < n; i++) {
-            if (isPal[0][i]) {
-                dp[i] = 0;
-            } else {
-                for (int j = 1; j <= i; j++) {
-                    if (isPal[j][i]) {
-                        dp[i] = min(dp[i], dp[j - 1] + 1);
-                    }
-                }
-            }
-        }
-        
-        return dp[n - 1];
+        return ans;
+    }
+
+    int minCut(string s) {
+        return solve(0, s);
     }
 };
+
+
+// class Solution {
+// public:
+//     int minCut(string s) {
+//         int n = s.size();
+        
+//         // isPal[i][j] = true if s[i..j] is palindrome
+//         vector<vector<bool>> isPal(n, vector<bool>(n, false));
+        
+//         // Precompute palindromes
+//         for (int len = 1; len <= n; len++) {
+//             for (int i = 0; i + len - 1 < n; i++) {
+//                 int j = i + len - 1;
+//                 if (s[i] == s[j]) {
+//                     if (len <= 2 || isPal[i + 1][j - 1])
+//                         isPal[i][j] = true;
+//                 }
+//             }
+//         }
+        
+//         // dp[i] = min cuts for s[0..i]
+//         vector<int> dp(n, INT_MAX);
+        
+//         for (int i = 0; i < n; i++) {
+//             if (isPal[0][i]) {
+//                 dp[i] = 0;
+//             } 
+//             else {
+//                 for (int j = 1; j <= i; j++) {
+//                     if (isPal[j][i]) {
+//                         dp[i] = min(dp[i], dp[j - 1] + 1);
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return dp[n - 1];
+//     }
+// };
