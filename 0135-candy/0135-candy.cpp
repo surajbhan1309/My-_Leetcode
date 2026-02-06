682f4e3ea7135da913d6cf15333b61ebd28b4d08
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-02-06 15:18:21 +0530

    Time: 0 ms (100%), Space: 22.2 MB (83.83%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        //2nd Approach peak and dip 
         int n=ratings.size();
-        vector<int>count(n,1);
+        int candy=n;
+        int i=1;
 
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                count[i]=max(count[i],count[i-1]+1);
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                continue;
             }
-        }
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                count[i]=max(count[i],count[i+1]+1);
+            int peak=0;
+            while(ratings[i]>ratings[i-1]){
+                i++;
+                peak++;
+                candy+=peak;
+                if(i==n){
+                    return candy;
+                }
             }
+            int dip=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                i++;
+                dip++;
+                candy+=dip;
+            }
+            candy-=min(peak,dip);  
         }
-        int result=0;
-        for(int i=0;i<n;i++){
-            result+=count[i];
-        }
-        return result;
+        return candy;
         
     }
 };
