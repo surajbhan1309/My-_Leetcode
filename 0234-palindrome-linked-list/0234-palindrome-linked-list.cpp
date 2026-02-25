10617e1083eb070aba3bcb091ce621e3de2316e6
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-02-25 16:34:16 +0530

    Time: 4 ms (51.42%), Space: 121.8 MB (48.63%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+  private:
+    ListNode* getMid(ListNode* head ) {
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+        
+        while(fast != NULL && fast-> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        
+        return slow;
+    }
+    ListNode* reverse(ListNode* head) {
+        
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(ListNode *head)
+    {
+        if(head -> next == NULL) {
+            return true;
+        }
+        
+        //step 1 -> find Middle
+        ListNode* mid=getMid(head);
+        
+        
+        //step2 -> reverse List after Middle
+        ListNode* temp=mid->next;
+        mid->next=reverse(temp);
+        
+        //step3 - Compare both halves
+
+        ListNode* head1=head;
+        ListNode* head2=mid->next;
+
+        while(head2!=NULL){
+            if(head1->val!=head2->val){
+                return false;
+            }
+            head1=head1->next;
+            head2=head2->next;
+        }
+
+        
+    
+        //step4 - repeat step 2
+        temp=mid->next;
+        mid->next=reverse(temp);
+
+        return true;
+    }
+};
