67dcc0ef07b0b41dc80e691575fd9c0a8b4bb79f
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-03-18 16:53:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -7,19 +7,22 @@ public:
 
         vector<vector<long long>> dp(m, vector<long long>(n, -1));
 
-        return uniquePaths(m - 1, n - 1, obstacleGrid, dp);
+        return solve(0, 0, obstacleGrid, dp);
     }
 
-    long long uniquePaths(int i, int j, vector<vector<int>> &grid, vector<vector<long long>> &dp) {
-        if (i < 0 || j < 0) return 0;
-        if (grid[i][j] == 1) return 0;
-        if (i == 0 && j == 0) return 1;
+    long long solve(int i, int j, vector<vector<int>> &grid, vector<vector<long long>> &dp) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if (i >= m || j >= n) return 0;     // out of bounds
+        if (grid[i][j] == 1) return 0;      // obstacle
+        if (i == m - 1 && j == n - 1) return 1;  // destination
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        long long up = uniquePaths(i - 1, j, grid, dp);
-        long long left = uniquePaths(i, j - 1, grid, dp);
+        long long down = solve(i + 1, j, grid, dp);
+        long long right = solve(i, j + 1, grid, dp);
 
-        return dp[i][j] = up + left;
+        return dp[i][j] = down + right;
     }
 };
