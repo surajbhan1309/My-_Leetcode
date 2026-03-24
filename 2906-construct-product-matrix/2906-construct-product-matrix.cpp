b0eea814ff53266d25d97d33929eaceb07a5a835
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-03-24 19:03:48 +0530

    Time: 38 ms (53.87%), Space: 132 MB (80.07%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        const int mod=12345, n=grid.size(), m=grid[0].size();
+        vector<vector<int>> ans(n, vector<int>(m));
+        long long pref=1, suf=1;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++){
+                ans[i][j]=pref;
+                pref=pref*grid[i][j]%mod;
+            }
+        for(int i=n-1; i>=0; i--)
+            for(int j=m-1; j>=0; j--){
+                ans[i][j]=ans[i][j]*suf%mod;
+                suf=suf*grid[i][j]%mod;
+            }
+        return ans;
+    }
+};
