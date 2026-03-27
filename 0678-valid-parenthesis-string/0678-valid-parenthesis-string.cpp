e4f67fef7c863a03731a8bffe33f26ff13e5706c
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-03-27 11:07:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n=s.size();
+        int low=0,high=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                low++;
+                high++;
+            }
+            else if(s[i]==')'){
+                low--;
+                if(low<0) low=0;
+                high--;
+            }
+            else{
+                low--;
+                if(low<0) low=0;
+                high++;
+            }
+            if(high<0) return false;
+        }
+        return low>=0;
+    }
+};
