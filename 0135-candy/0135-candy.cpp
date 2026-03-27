78ad64cb3516323737799ea27dfc484ca1494224
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-03-27 11:40:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -18,7 +18,7 @@ public:
                 if(i==n) return candy;
             }
             int dip=0;
-            while(ratings[i]<ratings[i-1]){
+            while(i<n && ratings[i]<ratings[i-1]){
                 i++;
                 dip++;
                 candy+=dip;
