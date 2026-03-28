3b3cfdd4d40a7c16eb969602b95ec0c98905171c
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-03-28 18:13:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int mergeSort(vector<int>& nums, int l, int r) {
+        if(l >= r) return 0;
+
+        int m = (l + r) / 2;
+        int cnt = 0;
+
+        cnt += mergeSort(nums, l, m);
+        cnt += mergeSort(nums, m+1, r);
+        cnt += countCross(nums, l, m, r);
+        merge(nums, l, m, r);
+
+        return cnt;
+    }
+
+    int countCross(vector<int>& nums, int l, int m, int r) {
+        int cnt = 0;
+        int j = m + 1;
+
+        for(int i = l; i <= m; i++) {
+            while(j <= r && (long long)nums[i] > 2LL * nums[j])
+                j++;
+            cnt += (j - (m + 1));
+        }
+        return cnt;
+    }
+
+    void merge(vector<int>& nums, int l, int m, int r) {
+        vector<int> temp;
+        int i = l, j = m+1;
+
+        while(i <= m && j <= r) {
+            if(nums[i] <= nums[j]) temp.push_back(nums[i++]);
+            else temp.push_back(nums[j++]);
+        }
+        while(i <= m) temp.push_back(nums[i++]);
+        while(j <= r) temp.push_back(nums[j++]);
+
+        for(int k = l; k <= r; k++) nums[k] = temp[k - l];
+    }
+
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums, 0, nums.size() - 1);
+    }
+};
