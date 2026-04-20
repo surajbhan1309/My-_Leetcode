263624380c353a4f9c61e6657bb5a2e53cadfc54
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-04-20 18:42:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -5,16 +5,18 @@ public:
         sort(nums.begin(),nums.end());
         int low=1;
         int high=nums[n-1];
+        int ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(Issumpossible(mid,nums,threshold)){
+                ans=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return low;
+        return ans;
         
     }
     bool Issumpossible(int mid,vector<int>& nums,int threshold){
