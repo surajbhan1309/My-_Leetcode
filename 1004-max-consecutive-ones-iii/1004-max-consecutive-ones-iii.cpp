359886a8e1e5ec7140aeb760125fc6f2b981850e
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-05-01 12:35:45 +0530

    Time: 4 ms (21.33%), Space: 69.7 MB (26.11%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,15 +4,15 @@ public:
         int left=0;
         int window=0;
         int ans=0;
-        int n=nums.size();
-        for(int right=0;right<n;right++){
-            window=window+nums[right];
+        for(int right=0;right<nums.size();right++){
+            window+=nums[right];
             while(window+k<(right-left+1)){
-                window=window-nums[left];
+                window-=nums[left];
                 left++;
             }
-            ans=max(ans,right-left+1);
+            ans=max(ans,(right-left+1));
         }
         return ans;
+        
     }
 };
