2000016e89d11a78c84c23bdceb8e2df933af4d2
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-05-03 15:29:35 +0530

    Time: 4 ms (47.36%), Space: 10 MB (10.62%) - LeetHub

---
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int SumofPrimes(int L, int R) {
+    vector<bool> prime(R + 1, true);
+    prime[0] = prime[1] = false;
+
+    for (int i = 2; i * i <= R; i++)
+        if (prime[i])
+            for (int j = i * i; j <= R; j += i)
+                prime[j] = false;
+
+    int sum= 0;
+    for (int i = L; i <= R; i++)
+        if (prime[i]) sum+=i;
+    return sum;
+}
+    int sumOfPrimesInRange(int n) {
+        string revstr=to_string(n);
+        reverse(revstr.begin(),revstr.end());
+        int digit=stoi(revstr);
+        if(digit<n) swap(digit,n);
+        return SumofPrimes(n,digit);
+    }
+};
