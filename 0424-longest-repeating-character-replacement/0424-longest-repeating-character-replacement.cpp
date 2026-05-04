c7d5cdde1a72be11763bb513c10c9dc774ca858b
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-05-04 15:58:33 +0530

    Time: 9 ms (32.19%), Space: 10.9 MB (39.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> count(26, 0);
-        int left = 0, right = 0;
-        int maxFreq = 0;
-        int ans = 0;
-        int n = s.size();
-        for (right = 0; right < n; right++) {
-            count[s[right] - 'A']++;
-            maxFreq = max(maxFreq, count[s[right] - 'A']);
-            while (maxFreq+k<(right-left+1)) {
-                count[s[left] - 'A']--;
+        unordered_map<char,int>map;
+        int left=0;
+        int maxfreq=0;
+        int ans=0;
+        int n=s.size();
+        for(int right=0;right<n;right++){
+            map[s[right]]++;
+            maxfreq=max(maxfreq,map[s[right]]);
+            while(maxfreq+k<(right-left+1)){
+                map[s[left]]--;
                 left++;
             }
-            ans = max(ans, right - left + 1);
+            ans=max(ans,right-left+1);
         }
         return ans;
+        
     }
 };
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         vector<int> count(26, 0);
+//         int left = 0;
+//         int maxfreq = 0;
+//         int ans = 0;
+//         int n = s.size();
+//         for(int right=0;right<n;right++){
+//             count[s[right]-'A']++;
+//             maxfreq=max(maxfreq,count[s[right]-'A']);
+//             while(maxfreq+k<(right-left+1)){
+//                 count[s[left]-'A']--;
+//                 left++;
+//             }
+//             ans=max(ans,right-left+1);
+//         }
+//         return ans;
+//     }
+// };
