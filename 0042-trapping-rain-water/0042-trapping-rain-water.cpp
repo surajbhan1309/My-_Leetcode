d849780a7940c867a220cbd6ddba698bdde73e41
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-05-05 22:08:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    //Two pointer Approach
     int trap(vector<int>& height) {
         int n=height.size();
-       int left=0,right=n-1;
-       int ans=0;
-       int lmax=0,rmax=0;
-       while(left<=right){
+        int left=0;
+        int right=n-1;
+        int ans=0;
+        int lmax=0,rmax=0;
+        while(left<=right){
             lmax=max(lmax,height[left]);
             rmax=max(rmax,height[right]);
             if(lmax<rmax){
@@ -17,8 +17,9 @@ public:
                 ans+=(rmax-height[right]);
                 right--;
             }
-       }
-       return ans;
+
+        }
+        return ans;
         
     }
 };
