b00da9f60cfda060a39696d1712f331e3748c8b9
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-05-07 22:36:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.py
+++ 3660-jump-game-ix/3660-jump-game-ix.py
@@ -1,25 +1,19 @@
 class Solution:
     def maxValue(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-
-        prefixmax = [0] * n
-        suffixmin = [0] * n
-        ans = [0] * n
-
-        prefixmax[0] = nums[0]
-        for i in range(1, n):
-            prefixmax[i] = max(prefixmax[i - 1], nums[i])
-
-        suffixmin[n - 1] = nums[n - 1]
-        for i in range(n - 2, -1, -1):
-            suffixmin[i] = min(suffixmin[i + 1], nums[i])
-
-        ans[n - 1] = prefixmax[n - 1]
-
-        for i in range(n - 2, -1, -1):
-            if prefixmax[i] > suffixmin[i + 1]:
-                ans[i] = ans[i + 1]
+        n=len(nums)
+        premax=[0]*n
+        suffmax=[0]*n
+        ans=[0]*n
+        premax[0]=nums[0]
+        for i in range(1,n):
+            premax[i]=max(premax[i-1],nums[i])
+        suffmax[n-1]=nums[n-1]
+        for i in range(n-2,-1,-1):
+            suffmax[i]=min(suffmax[i+1],nums[i])
+        ans[n-1]=premax[n-1]
+        for i in range(n-2,-1,-1):
+            if(premax[i]>suffmax[i+1]):
+                ans[i]=ans[i+1]
             else:
-                ans[i] = prefixmax[i]
-
+                ans[i]=premax[i]
         return ans
