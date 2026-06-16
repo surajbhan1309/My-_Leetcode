41ade5608bf9e5f296fbef5fbe574903e3ead460
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-16 23:12:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& state) {
+        if (state[node] == 1) return false; // cycle found
+        if (state[node] == 2) return true;  // already processed
+
+        state[node] = 1; // visiting
+
+        for (int i : adj[node]) {
+            if (!dfs(i, adj, state))
+                return false;
+        }
+
+        state[node] = 2; // visited
+        return true;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        vector<int>indeg(n,0);
-        int count=0;
-        vector<vector<int>>adj(n);
-        queue<int>q;
-        for(auto x:prerequisites){
-            adj[x[1]].push_back(x[0]);
-            indeg[x[0]]++;
+        vector<vector<int>> adj(numCourses);
+
+        for (auto &p : prerequisites) {
+            adj[p[0]].push_back(p[1]);
         }
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0) q.push(i);
+
+        vector<int> state(numCourses, 0);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, adj, state))
+                return false;
         }
-        while(!q.empty()){
-            int u=q.front();
-            q.pop();
-            count++;
-            for(auto v:adj[u]){
-                indeg[v]--;
-                if(indeg[v]==0){
-                    q.push(v);
-                }
-            }
-        }
-        return count==n;
+
+        return true;
     }
 };
