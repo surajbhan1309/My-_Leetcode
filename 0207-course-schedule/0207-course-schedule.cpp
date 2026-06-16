3ec36e8fde0c4bcb9349195fe33addb6b54011ef
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-16 23:31:26 +0530

    Time: 3 ms (75.31%), Space: 19.2 MB (67.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
-        vis[node] = 1; // visiting
-
-        for (int next : adj[node]) {
-            if (vis[next] == 1) return false; // cycle
-
-            if (vis[next] == 0) {
-                if (!dfs(next, adj, vis))
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        vector<int>vis(n,0);
+        vector<vector<int>>adj(n);
+        for(auto &x:prerequisites){
+            adj[x[1]].push_back(x[0]);
+        }
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                if(!dfs(i,adj,vis)){ // can be written as dfs(i,adj,vis)==false
                     return false;
+                }
             }
         }
-
-        vis[node] = 2; // done
         return true;
     }
-
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-
-        for (auto &p : prerequisites)
-            adj[p[0]].push_back(p[1]);
-
-        vector<int> vis(numCourses, 0);
-
-        for (int i = 0; i < numCourses; i++) {
-            if (vis[i] == 0) {
-                if (dfs(i, adj, vis)==false)
+    bool dfs(int node,vector<vector<int>>&adj,vector<int>&vis){
+        vis[node]=1;
+        for(auto &nbr:adj[node]){
+            if(vis[nbr]==1) return false; // cycle detect
+            if(vis[nbr]==0){
+                if(!dfs(nbr,adj,vis)){
                     return false;
+                }
             }
         }
-
+        vis[node]=2;
         return true;
     }
 };
