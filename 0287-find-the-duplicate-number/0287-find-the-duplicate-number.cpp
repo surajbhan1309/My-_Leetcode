f1ce07b66b7ab13bba9ba9bf45a81b99c5334379
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-17 12:29:45 +0530

    Time: 0 ms (100%), Space: 65 MB (69.47%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-        
-        // Phase 1: Detect cycle
+        int slow = 0;
+        int fast = 0;
+
         do {
             slow = nums[slow];
-            fast = nums[nums[fast]];
+            fast =nums[nums[fast]];
         } while (slow != fast);
-        
-        // Phase 2: Find entry point
-        fast = nums[0];
+
+        fast = 0;
         while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
-        
-        return slow;
+        return fast;
+
+
     }
 };
