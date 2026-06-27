7af0fb633fa02a850956b4da699b45b7953ec9ed
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-27 12:18:15 +0530

    Time: 22 ms (61.22%), Space: 139.2 MB (42.43%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,15 +5,19 @@ public:
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
             //Remove elements out of window
-            if (!dq.empty() && dq.front()<=i-k) 
+            if(!dq.empty() && dq.front()<=i-k){
                 dq.pop_front();
+            }
             //Remove smaller elements from back
-            while (!dq.empty() && nums[dq.back()]<=nums[i])
+            while(!dq.empty() && nums[dq.back()]<=nums[i]){
                 dq.pop_back();
-            dq.push_back(i); //Add current index
+            }
+            //Add current index
+            dq.push_back(i); 
             //Store answer once first window is formed
-            if(i>=k-1)
+            if(i>=k-1){
                 ans.push_back(nums[dq.front()]);
+            }
         }
         return ans;
     }
