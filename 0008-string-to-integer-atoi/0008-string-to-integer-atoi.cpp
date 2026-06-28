e6c642c376c1850b9a91f2f96d37dd6d0f934667
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-28 20:03:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (11.37%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -6,19 +6,18 @@ public:
         int i=0;
         while(i<n && s[i]==' ') i++;
         s=s.substr(i);
-        int sign=+1;
         long long ans=0;
+        int sign=+1;
         if(s[0]=='-') sign=-1;
-        int maxi=INT_MAX,mini=INT_MIN;
         i=(s[0]=='+' || s[0]=='-')?1:0;
-        while(i<s.size()){//loop with updated size of string
-            if(s[0]==' ' || !isdigit(s[i])) break;
-            ans=ans*10+s[i]-'0';
-            //check overflow
+        int maxi=INT_MAX,mini=INT_MIN;
+        while(i<s.size()){
+            if(s[0]==' '|| !isdigit(s[i])) break;
+            ans=10*ans+(s[i]-'0');
             if(sign==-1 && -1*ans<mini) return mini;
-            if(sign==1 && ans>maxi) return maxi;
+            if(sign==1 && 1*ans>maxi) return maxi;
             i++;
         }
-        return (int)(sign*ans);
+        return (int)(ans*sign);
     }
 };
