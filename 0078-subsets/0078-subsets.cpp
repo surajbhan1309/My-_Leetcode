fa7608f192a9b3886165d5193c37a659bba2a4e2
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-30 11:27:49 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.17%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size();
         int total=1<<n;
         vector<vector<int>>ans;
-
         for(int mask=0;mask<total;mask++){
             vector<int>subset;
             for(int i=0;i<n;i++){
@@ -15,39 +14,6 @@ public:
             ans.push_back(subset);
         }
         return ans;
-
         
     }
 };
-
-
-
-
-
-
-// class Solution {
-// public:
-
-//     void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
-
-//         if (index == nums.size()) {
-//             ans.push_back(output);
-//             return ;
-//         }
-
-//         // excluding the current element
-//         solve(nums, output, index+1, ans);
-
-//         // including the current element
-//         int element = nums[index];
-//         output.push_back(element);
-//         solve(nums, output, index+1, ans);
-//     }
-//     vector<vector<int> > subsets(vector<int>& nums) {
-//         vector<vector<int>> ans;
-//         vector<int> output;
-//         int index = 0;
-//         solve(nums, output, index, ans);
-//         return ans;
-//     }
-// };
