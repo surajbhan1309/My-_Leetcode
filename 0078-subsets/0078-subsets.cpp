bc73dc3f3fc574d3e6bbdef3766d9e10d143b334
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-30 11:46:23 +0530

    Time: 2 ms (38.16%), Space: 10 MB (38.25%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
+        int total=1<<n;
         vector<vector<int>>ans;
-        vector<int>path;
-        solve(nums,ans,path,0);
-        return ans;
-    }
-    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&path,int i){
-        if(i==nums.size()){
-            ans.push_back(path);
-            return;
+        for(int mask=0;mask<total;mask++){
+            vector<int>subset;
+            for(int i=0;i<n;i++){
+                if(mask & (1<<i)){
+                    subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subset);
         }
-        path.push_back(nums[i]);
-        solve(nums,ans,path,i+1);
-        path.pop_back();
-        solve(nums,ans,path,i+1);
+        return ans;
+        
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>>ans;
+//         vector<int>path;
+//         solve(nums,ans,path,0);
+//         return ans;
+//     }
+//     void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&path,int i){
+//         if(i==nums.size()){
+//             ans.push_back(path);
+//             return;
+//         }
+//         path.push_back(nums[i]);
+//         solve(nums,ans,path,i+1);
+//         path.pop_back();
+//         solve(nums,ans,path,i+1);
+//     }
+// };
