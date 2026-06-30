249e95ffdce91247c78802a4860d0905425e2457
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-30 11:07:34 +0530

    Time: 0 ms (100%), Space: 14.1 MB (75.56%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -7,16 +7,32 @@ public:
         backtrack(ans,target,candidates,path,0);
         return ans;
     }
-    void backtrack(vector<vector<int>>&ans,int target,vector<int>&candidates,vector<int>&path,int start){
+    // void backtrack(vector<vector<int>>&ans,int target,vector<int>&candidates,vector<int>&path,int start){
+    //     if(target==0){
+    //         ans.push_back(path);
+    //         return ;
+    //     }
+    //     for(int i=start;i<candidates.size();i++){
+    //         if(candidates[i]>target) continue;
+    //         path.push_back(candidates[i]);
+    //         backtrack(ans,target-candidates[i],candidates,path,i);
+    //         path.pop_back();
+    //     }
+    // }
+    void backtrack(vector<vector<int>>&ans,int target,vector<int>&candidates,vector<int>&path,int i){
         if(target==0){
             ans.push_back(path);
             return ;
         }
-        for(int i=start;i<candidates.size();i++){
-            if(candidates[i]>target) continue;
+        if(i==candidates.size()){
+            return;
+        }
+        if(candidates[i]<=target){
             path.push_back(candidates[i]);
             backtrack(ans,target-candidates[i],candidates,path,i);
             path.pop_back();
         }
+        backtrack(ans,target,candidates,path,i+1);
+
     }
 };
