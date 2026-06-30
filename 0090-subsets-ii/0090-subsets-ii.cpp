5153d99e5d277578cffcb19a799eb29e30c31117
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-30 11:57:57 +0530

    Time: 0 ms (100%), Space: 10.3 MB (84.82%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        vector<int>path;
-        solve(nums,ans,path,0);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> path;
+        solve(nums, ans, path, 0);
         return ans;
     }
-    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&path,int i){
-        if(i==nums.size()){
-            ans.push_back(path);
-            return;
+
+    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& path, int start) {
+        ans.push_back(path);
+
+        for (int i = start; i < nums.size(); ++i) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            path.push_back(nums[i]);
+            solve(nums, ans, path, i + 1);
+            path.pop_back();
         }
-        path.push_back(nums[i]);
-        solve(nums,ans,path,i+1);
-        path.pop_back();
-        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
-            i++;
-        }
-        solve(nums,ans,path,i+1);
     }
 };
