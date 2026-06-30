777e9abe77aa68c4153c36eee40b5bf0f1dbddfb
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-30 11:58:56 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.26%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -20,3 +20,50 @@ public:
         }
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         int n=nums.size();
+//         sort(nums.begin(),nums.end());
+//         vector<vector<int>>ans;
+//         vector<int>path;
+//         solve(nums,ans,path,0);
+//         return ans;
+//     }
+//     void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&path,int i){
+//         if(i==nums.size()){
+//             ans.push_back(path);
+//             return;
+//         }
+//         path.push_back(nums[i]);
+//         solve(nums,ans,path,i+1);
+//         path.pop_back();
+//         while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+//             i++;
+//         }
+//         solve(nums,ans,path,i+1);
+//     }
+// };
+// class Solution {
+// public:
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         set<vector<int>>st;
+//         int n=nums.size();
+//         sort(nums.begin(),nums.end());
+
+//         for(int mask=0;mask<(1<<n);mask++){
+//             vector<int>subset;
+//             for(int i=0;i<n;i++){
+//                 if(mask&(1<<i)){
+//                     subset.push_back(nums[i]);
+//                 }
+//             }
+//             st.insert(subset);
+//         }
+//         vector<vector<int>>ans(st.begin(),st.end());
+//         return ans;
+
+        
+//     }
+// };
+
