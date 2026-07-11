cb4b59e8e282c923ba9a98a0309882d5da4177a1
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-07-11 18:27:54 +0530

    Time: 12 ms (14.27%), Space: 20.2 MB (24.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
-        vector<int>vis(n,0);
         vector<vector<int>>adj(n);
-        for(auto &x:prerequisites){
+        int count=0;
+        vector<int>indeg(n,0);
+        for(auto x:prerequisites){
             adj[x[1]].push_back(x[0]);
+            indeg[x[0]]++;
         }
+        queue<int>q;
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                if(!dfs(i,adj,vis)){ // can be written as dfs(i,adj,vis)==false
-                    return false;
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            count++;
+
+            for(auto v:adj[u]){
+                indeg[v]--;
+                if(indeg[v]==0){
+                    q.push(v);
                 }
             }
         }
-        return true;
-    }
-    bool dfs(int node,vector<vector<int>>&adj,vector<int>&vis){
-        vis[node]=1;
-        for(auto &nbr:adj[node]){
-            if(vis[nbr]==1) return false; // cycle detect
-            if(vis[nbr]==0){
-                if(!dfs(nbr,adj,vis)){
-                    return false;
-                }
-            }
-        }
-        vis[node]=2;
-        return true;
+        return count==n;
     }
 };
