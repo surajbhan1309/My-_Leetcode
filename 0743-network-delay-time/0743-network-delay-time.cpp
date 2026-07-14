414fa842a810eb00f9e5d9fd16e176eecad3d43e
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-07-14 19:34:54 +0530

    Time: 70 ms (95.31%), Space: 44.2 MB (89.86%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        // adjacency list: u -> (v, w)
+        vector<vector<pair<int,int>>> adj(n+1);
+        for (auto &time : times) {
+            adj[time[0]].push_back({time[1], time[2]});
+        }
+
+        // min-heap: {time, node}
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> dist(n+1, INT_MAX);
+
+        dist[k] = 0;
+        pq.push({0, k});
+
+        while (!pq.empty()) {
+            auto [currTime, u] = pq.top();
+            pq.pop();
+
+            for (auto [v, w] : adj[u]) {
+                if (currTime + w < dist[v]) {
+                    dist[v] = currTime + w;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == INT_MAX) return -1;
+            ans = max(ans, dist[i]);
+        }
+        return ans;
+    }
+};
