7c5a02a32a6526afcc238951eeca1f8284f6a058
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-07-14 12:25:45 +0530

    Time: 10 ms (91.14%), Space: 25.3 MB (58.23%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        if(m==0 || n==0 || grid[0][0]!=0){
-            return -1;
-        }
-        int delrow[]={-1,-1,-1,0,0,1,1,1};
-        int delcol[]={-1,0,1,-1,1,-1,0,1};
+        int n=grid.size();
+        int m=grid[0].size();
+
+        if(grid[0][0]==1) return -1;
+
+        int dr[]={-1,-1,-1,0,0,1,1,1};
+        int dc[]={-1,0,1,-1,1,-1,0,1};
 
         queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         q.push({0,0});
-        grid[0][0]=1;
+        vis[0][0]=1;
         int steps=1;
-
         while(!q.empty()){
-            int ns=q.size();
-            while(ns--){
-                auto curr=q.front();
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                auto[row,col]=q.front();
                 q.pop();
-                int x=curr.first;
-                int y=curr.second;
 
-                if(x==m-1 && y==n-1){
-                    return steps;
-                }
+                if(row==n-1 && col==m-1) return steps;
+
                 for(int k=0;k<8;k++){
-                    int nx=x+delrow[k];
-                    int ny=y+delcol[k];
+                    int nrow=row+dr[k];
+                    int ncol=col+dc[k];
 
-                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==0){
-                        q.push({nx,ny});
-                        grid[nx][ny]=1;
-                    } 
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0 && !vis[nrow][ncol]){
+                        q.push({nrow,ncol});
+                        vis[nrow][ncol]=1;
+                    }
                 }
             }
             steps++;
         }
         return -1;
-        
     }
 };
