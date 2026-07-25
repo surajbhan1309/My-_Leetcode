eb867180a9929e2077bf31d906564077a8206daf
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-07-25 11:34:40 +0530

    Time: 495 ms (62.88%), Space: 243.6 MB (17.11%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,51 +1,59 @@
 class Solution {
 public:
-    int mergeSort(vector<int>& nums, int l, int r) {
-        if(l >= r) return 0;
-
-        int m = (l + r) / 2;
-        int cnt = 0;
-
-        cnt += mergeSort(nums, l, m);
-        cnt += mergeSort(nums, m+1, r);
-        cnt += countCross(nums, l, m, r);
-        merge(nums, l, m, r);
-
-        return cnt;
-    }
-
-    int countCross(vector<int>& nums, int l, int m, int r) {
-        int cnt = 0;
-        int j = m + 1;
-
-        for(int i = l; i <= m; i++) {
-            while(j <= r && (long long)nums[i] > 2LL * nums[j])
-                j++;
-            cnt += (j - (m + 1));
-        }
-        return cnt;
-        // (m+1) = starting index of right half
-        // j stops where condition breaks
-
-        // So:
-        // 👉 number of valid elements = j - (m+1)
-    }
-
-    void merge(vector<int>& nums, int l, int m, int r) {
-        vector<int> temp;
-        int i = l, j = m+1;
-
-        while(i <= m && j <= r) {
-            if(nums[i] <= nums[j]) temp.push_back(nums[i++]);
-            else temp.push_back(nums[j++]);
-        }
-        while(i <= m) temp.push_back(nums[i++]);
-        while(j <= r) temp.push_back(nums[j++]);
-
-        for(int k = l; k <= r; k++) nums[k] = temp[k - l];
-    }
-
     int reversePairs(vector<int>& nums) {
-        return mergeSort(nums, 0, nums.size() - 1);
+        return mergesort(nums, 0, nums.size() - 1);
+    }
+
+    int mergesort(vector<int>& nums, int low, int high) {
+        if (low >= high) return 0;
+        int mid = low + (high - low) / 2;
+        int count = 0;
+        
+        count += mergesort(nums, low, mid);
+        count += mergesort(nums, mid + 1, high);
+        count += countPairs(nums, low, mid, high); // 1. Count pairs first
+        merge(nums, low, mid, high);               // 2. Then sort/merge
+        
+        return count;
+    }
+
+    int countPairs(vector<int>& nums, int low, int mid, int high) {
+        int count = 0;
+        int right = mid + 1;
+        
+        for (int left = low; left <= mid; left++) {
+            // Use long long casting to prevent integer overflow errors
+            while (right <= high && (long long)nums[left] > 2 * (long long)nums[right]) {
+                right++;
+            }
+            count += (right - (mid + 1));
+        }
+        return count;
+    }
+
+    void merge(vector<int>& nums, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+
+        // Standard merge sort operation
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp.push_back(nums[left++]);
+            } else {
+                temp.push_back(nums[right++]);
+            }
+        }
+
+        while (left <= mid) {
+            temp.push_back(nums[left++]);
+        }
+        while (right <= high) {
+            temp.push_back(nums[right++]);
+        }
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
     }
 };
