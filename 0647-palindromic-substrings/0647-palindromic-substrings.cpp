3d842840296d0415a8f4c567a33e4714ecdaaaea
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-05 09:50:06 +0530

    Time: 1 ms (91.94%), Space: 8.6 MB (60.62%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
+    int count;
     int countSubstrings(string s) {
+        count=0;
         int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int count=0;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(ispal(dp,s,i,j)){
-                    count++;
-                }
-            }
+            check(i,i,s,n);
+            check(i,i+1,s,n);
         }
         return count;
     }
-    int ispal(vector<vector<int>>&dp,string &s,int i,int j){
-        if(i==j) return 1;
-        if(j==i+1 && s[i]==s[j]) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        dp[i][j]=((s[i]==s[j]) && ispal(dp,s,i+1,j-1));
-        return dp[i][j];
+    void check(int i,int j,string &s,int n){
+        while(i>=0 && j<n && s[i]==s[j]){
+            count++;
+            i--;
+            j++;
+        }
     }
 };
+
+// class Solution {
+// public:
+//     int countSubstrings(string s) {
+//         int n=s.size();
+//         vector<vector<int>>dp(n,vector<int>(n,-1));
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(ispal(dp,s,i,j)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+//     int ispal(vector<vector<int>>&dp,string &s,int i,int j){
+//         if(i==j) return 1;
+//         if(j==i+1 && s[i]==s[j]) return 1;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         dp[i][j]=((s[i]==s[j]) && ispal(dp,s,i+1,j-1));
+//         return dp[i][j];
+//     }
+// };
