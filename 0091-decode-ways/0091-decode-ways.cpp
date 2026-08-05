52be549f8506aae64c41939b957555dbb97ffdf1
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-05 10:15:16 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.11%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(int i, string &s, int &n) {
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-
-        if(i == n) {
-            return dp[i] = 1; //one valid split done
-        }
-
-        if(s[i] == '0') {
-            return dp[i] = 0; //not possible to split
-        }
-
-        dp[i]= solve(i+1, s, n);
-        
-        if(i+1 < n) {
-            if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
-                dp[i]+= solve(i+2, s, n);
-        }
-
-        
-        return dp[i];
-
-    }
-
     int numDecodings(string s) {
-        int n = s.length();
-        
-        memset(dp, -1, sizeof(dp));
-        return solve(0, s, n);
-        
-
+        int n=s.size();
+        vector<int>dp(n+1,-1);
+        return solve(dp,s,0,n);
+    }
+    int solve(vector<int>&dp,string &s,int i,int n){
+        if(dp[i]!=-1) return dp[i];
+        if(i==n) return 1;
+        if(s[i]=='0') return 0;
+        dp[i]=solve(dp,s,i+1,n);
+        if(i+1<n){
+            if(s[i]=='1' || (s[i]=='2' && s[i+1]<='6')){
+                dp[i]+=solve(dp,s,i+2,n);
+            }
+        }
+        return dp[i];
     }
 };
