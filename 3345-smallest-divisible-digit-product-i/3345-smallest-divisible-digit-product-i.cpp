3431388e406e459ef73267d5c85321859bf269d4
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-06 17:21:01 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.77%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
+    bool isValid(int num, int t) {
+        int product = 1;
+        while (num > 0) {
+            product *= (num % 10);
+            num /= 10;
+        }
+        return (product % t == 0);
+    }
+
     int smallestNumber(int n, int t) {
-        auto check = [&](int num) -> bool {
-            int product = 1;
-            while (num) {
-                product *= (num % 10);
-                num /= 10;
-                if (!product) {
-                    break;
-                }
-            }
-            return !(product % t);
-        };
-        while (!check(n)) {
+        while (!isValid(n, t)) {
             n++;
         }
         return n;
