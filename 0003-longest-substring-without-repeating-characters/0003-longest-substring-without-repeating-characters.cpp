2166bb8f88fa331ecd01aadc617ff6d986fbbc7e
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-08 17:40:03 +0530

    Time: 5 ms (71.67%), Space: 18.3 MB (19.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,11 +4,11 @@ public:
         int n=s.size();
         int max_len=0;
         int left=0;
-        vector<int>mp(26,0);
+        vector<int>mp(128,0);//ascii
         for(int right=0;right<n;right++){
-            mp[s[right]-'a']++;
-            while(mp[s[right]-'a']>1){
-                mp[s[left]-'a']--;
+            mp[s[right]]++;
+            while(mp[s[right]]>1){
+                mp[s[left]]--;
                 left++;
             }
             max_len=max(max_len,right-left+1);
