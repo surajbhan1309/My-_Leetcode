34fe2a68dce584cf697ca7142110b2cf249681db
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-10 16:40:07 +0530

    Time: 0 ms (100%), Space: 77 MB (76.4%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,115 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode();
-        ListNode* res = dummy;
-        int total = 0, carry = 0;
+        ListNode* dummy=new ListNode();
+        ListNode* temp=dummy;
+        int total=0;
+        int carry=0;
 
-        while (l1 || l2 || carry) {
-            total = carry;
-
-            if (l1) {
-                total += l1->val;
-                l1 = l1->next;
+        while(l1||l2||carry){
+            total=carry;
+            if(l1){
+                total+=l1->val;
+                l1=l1->next;
             }
-            if (l2) {
-                total += l2->val;
-                l2 = l2->next;
+            if(l2){
+                total+=l2->val;
+                l2=l2->next;
             }
-
-            int num = total % 10;
-            carry = total / 10;
-            dummy->next = new ListNode(num);
-            dummy = dummy->next;
+            int num=total%10;
+            carry=total/10;
+            temp->next=new ListNode(num);
+            temp=temp->next;
         }
-
-        ListNode* result = res->next;
-        delete res;
-        return result;         
+        return dummy->next;
     }
 };
-// class Solution
-// {
-// public:
-//     // Function to reverse a linked list
-//     ListNode* reverse(ListNode* head) {
-//         ListNode* prev = nullptr;
-//         ListNode* curr = head;
-//         ListNode* nextNode = nullptr;
-        
-//         while (curr != nullptr) {
-//             nextNode = curr->next;
-//             curr->next = prev;
-//             prev = curr;
-//             curr = nextNode;
-//         }
-//         return prev;
-//     }
-    
-//     // Function to insert nodes at tail
-//     void insertAtTail(ListNode* &head, ListNode* &tail, int val) {
-//         ListNode* temp = new ListNode(val);
-        
-//         // If the list is already empty
-//         if (head == nullptr) {
-//             head = temp;
-//             tail = temp;
-//         } else {
-//             tail->next = temp;
-//             tail = temp;
-//         }
-//     }
-    
-//     ListNode* add(ListNode* l1, ListNode* l2) {
-//         int carry = 0;
-        
-//         ListNode* ansHead = nullptr;
-//         ListNode* ansTail = nullptr;
-        
-//         while (l1 != nullptr || l2 != nullptr || carry != 0) {
-//             int val1 = 0;
-//             if (l1 != nullptr) {
-//                 val1 = l1->val;
-//             }
-            
-//             int val2 = 0;
-//             if (l2 != nullptr) {
-//                 val2 = l2->val;
-//             }
-            
-//             int sum = carry + val1 + val2;
-//             int digit = sum % 10;
-            
-//             // Adding nodes to the result list
-//             insertAtTail(ansHead, ansTail, digit);
-//             carry = sum / 10;
-            
-//             if (l1 != nullptr) {
-//                 l1 = l1->next;
-//             }
-            
-//             if (l2 != nullptr) {
-//                 l2 = l2->next;
-//             }
-//         }
-        
-//         return ansHead;
-//     }
-    
-//     // Function to add two numbers represented by linked lists
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-       
-        
-//         // Step 1: Add the two reversed linked lists
-//         ListNode* ans = add(l1, l2);
-        
-//         // Step 2: Reverse the result to get the final answer
-//         //ans = reverse(ans);
-        
-        
-        
-//         return ans;
-//     }
-// };
-
-
