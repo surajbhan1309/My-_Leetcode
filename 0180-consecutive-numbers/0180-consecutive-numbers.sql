3dcecca8a643bb9cb2e010ff0861398ef8b3f79a
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-10 10:32:46 +0530

    Time: 833 ms (9.19%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,13 +1,7 @@
 # Write your MySQL query statement below
-SELECT DISTINCT
-    l1.Num AS ConsecutiveNums
-FROM
-    Logs l1,
-    Logs l2,
-    Logs l3
-WHERE
-    l1.Id = l2.Id - 1
-    AND l2.Id = l3.Id - 1
-    AND l1.Num = l2.Num
-    AND l2.Num = l3.Num
-;
+select distinct l1.num as ConsecutiveNums
+from Logs l1,Logs l2,Logs l3
+where l1.id=l2.id-1
+and l2.id=l3.id-1
+and l1.num=l2.num
+and l2.num=l3.num
