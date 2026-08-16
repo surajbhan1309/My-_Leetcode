8b5abd97df04d77093f0b7b40081a997278d3154
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-16 15:52:54 +0530

    Time: 0 ms (100%), Space: 28.2 MB (43.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    // int majorityElement(vector<int>& nums) {
-    //     // sort(nums.begin(),nums.end());
-    //     // int n=nums.size();
-    //     // return nums[n/2];
-    // }
-    //Moore voting algo 
     int majorityElement(vector<int>& nums) {
-        int count = 1;
-        int element = nums[0];
-        int n = nums.size();
+        int n=nums.size();
+        int element=nums[0];
+        int count=0;
 
-        // Find candidate
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == element)
+        for(auto &num:nums){
+            if(num==element){
                 count++;
-            else if (count == 0) {
-                element = nums[i];
-                count = 1;
-            } else {
+            }
+            else if(count==0){
+                element=num;
+                count=1;
+            }
+            else{
                 count--;
             }
         }
-
-        // Verification step
-        count = 0;
-        for (int x : nums) {
-            if (x == element)
+        count=0;
+        for(int x:nums){
+            if(x==element){
                 count++;
+            }
         }
-
-        if (count > n / 2)
+        if(count>n/2){
             return element;
-
-        return -1; // No majority element exists
+        }
+        return -1;
     }
 };
