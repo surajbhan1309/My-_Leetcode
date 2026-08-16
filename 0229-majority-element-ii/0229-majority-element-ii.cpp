ac4ea1a6c2ad56a6394eef75f1aefaa6b4df827d
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-16 16:07:39 +0530

    Time: 0 ms (100%), Space: 23.6 MB (77.21%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //in n/3 majority max 2 can be major 
-        //further like for n/4 is 3 and n/5 is 4 and so on
         int n=nums.size();
+        int maj1=nums[0],maj2=nums[0];
         int count1=0,count2=0;
-        int maj1=0,maj2=1;
-         // different initial values
-        
-        // Step 1: Find candidates
-        for(int num:nums){
+        for(auto &num:nums){
             if(num==maj1) count1++;
             else if(num==maj2) count2++;
             else if(count1==0){
@@ -25,8 +20,6 @@ public:
                 count2--;
             }
         }
-
-        // Step 2: Verify candidates
         int c1=0,c2=0;
         for(int num:nums){
             if(num==maj1) c1++;
