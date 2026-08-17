eab52421da8128f43d41f68b78a91245524c83ff
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-17 23:18:38 +0530

    Time: 31 ms (27.89%), Space: 17.5 MB (42.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,9 +3,7 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-
         vector<vector<int>>vis(n,vector<int>(m,0));
-
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -21,56 +19,12 @@ public:
         vis[i][j]=1;
         int dr[]={-1,0,1,0};
         int dc[]={0,1,0,-1};
-
         for(int k=0;k<4;k++){
             int nrow=i+dr[k];
             int ncol=j+dc[k];
-
             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
                 dfs(grid,vis,nrow,ncol,n,m);
             }
         }
-        
     }
 };
-// class Solution {
-// public:
-//     int numIslands(vector<vector<char>>& grid) {
-//         if (grid.empty() || grid[0].empty()) return 0;
-        
-//         int n = grid.size();
-//         int m = grid[0].size();
-//         int count = 0;
-        
-//         int dr[] = {-1, 0, 1, 0};
-//         int dc[] = {0, 1, 0, -1};
-        
-//         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < m; j++) {
-//                 if (grid[i][j] == '1') {
-//                     count++;
-                    
-//                     queue<pair<int, int>> q;
-//                     q.push({i, j});
-//                     grid[i][j] = '0';
-                    
-//                     while (!q.empty()) {
-//                         auto [r, c] = q.front();
-//                         q.pop();
-                        
-//                         for (int k = 0; k < 4; k++) {
-//                             int nrow = r + dr[k];
-//                             int ncol = c + dc[k];
-                            
-//                             if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1') {
-//                                 q.push({nrow, ncol});
-//                                 grid[nrow][ncol] = '0';
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// };
