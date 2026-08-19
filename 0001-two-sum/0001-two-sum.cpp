c0f74b300653cad1d654b17f923ffb0c7c83a826
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-19 22:08:17 +0530

    Time: 5 ms (43.19%), Space: 15 MB (25.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,12 +5,11 @@ public:
         unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
             int complement=target-nums[i];
-            if(mp.find(complement)!=mp.end()){
-                return {mp[complement],i};
+            if(mp.count(complement)){
+                return {i,mp[complement]};
             }
             mp[nums[i]]=i;
         }
         return {};
-        
     }
 };
