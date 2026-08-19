0bb70f8954269b858e45cb6eab67b9ac0927dee4
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-19 19:35:51 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,16 +3,10 @@ public:
     bool isAnagram(string s, string t) {
         vector<int>f(26,0);
         if(s.size()!=t.size()) return false;
-        for(auto &c:s){
-            f[c-'a']++;
-        }
-        for(auto &c:t){
-            f[c-'a']--;
-        }
+        for(auto &c:s) f[c-'a']++;
+        for(auto &c:t) f[c-'a']--;
         for(auto &count:f){
-            if(count!=0){
-                return false;
-            }
+            if(count!=0) return false;
         }
         return true;
     }
