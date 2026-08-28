f4f00f98bc8194ec0c0c11d4251219823420a5dd
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-28 14:41:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.4%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,16 +1,25 @@
 class Solution {
+private:
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            totalSum += digit * digit;
+            n /= 10;
+        }
+        return totalSum;
+    }
+
 public:
     bool isHappy(int n) {
-        if(n==1 || n==7) return true;
-        else if(n<10) return false;
-        else{
-            int sum=0;
-            while(n>0){
-                int temp=n%10;
-                sum+= temp*temp;
-                n=n/10;
-            }
-            return isHappy(sum);
+        int slow = n;
+        int fast = getNext(n);
+        
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
         }
+        
+        return fast == 1;
     }
 };
