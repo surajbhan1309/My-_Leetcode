e6498c99d6c49fda96924af2b4f8f823a5ff332f
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-09-06 12:22:47 +0530

    Time: 3 ms (47.02%), Space: 10.4 MB (16.19%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     bool isPalindromic(string s) {
         string str="";
-       for(int i=0;i<s.size();i++){
-           int a=s[i];
-     str+=bitset<8>(a).to_string();
-       
-       }
-       string rev=str;
-       reverse(rev.begin(),rev.end());
-       if(rev == str)
-        return true;
+        for(int i=0;i<s.size();i++){
+            int a=s[i];
+            str+=bitset<8>(a).to_string();
+        }
+        string rev=str;
+        reverse(rev.begin(),rev.end());
+        if(rev == str)
+            return true;
         return false;
     }
 };
