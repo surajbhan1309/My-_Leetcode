f6767aaa3baa350661457e2b329a37eff6679293
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-09-13 16:29:10 +0530

    Time: 7 ms (50%), Space: 32.4 MB (100%) - LeetHub

--- 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -1,30 +1,24 @@
-class Solution {
-public:
-    int countSpecialIntegers(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        vector<bool> visited(n, false);
+class Solution { 
+public: 
+    int countSpecialIntegers(vector<int>& nums) { 
+        map<int, vector<int>> mp; 
+        int ans = 0; 
+        int n = nums.size(); 
         
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            
-            vector<int> indices;
-            for (int j = i; j < n; j++) {
-                if (nums[j] == nums[i]) {
-                    indices.push_back(j);
-                    visited[j] = true;
-                }
-            }
+        for(int i = 0; i < n; i++){ 
+            mp[nums[i]].push_back(i); 
+        } 
+        
+        for(auto &x : mp){ 
+            vector<int>& indices = x.second;
             
             if (indices.size() == 3) {
                 if (indices[1] - indices[0] == indices[2] - indices[1]) {
                     ans++;
                 }
             }
-        }
+        } 
         
         return ans;
-    }
+    } 
 };
